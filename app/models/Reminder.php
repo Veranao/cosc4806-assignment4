46d19f8a16bc591746c04be7c09e53e0a9838d69
Veranao <64379851+Veranao@users.noreply.github.com>
2025-07-04 02:58:43 +0000

    created reminder class connecting to reminders table

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -8,8 +8,13 @@ class Reminder {
 
     public function get_all_reminders () {
       $db = db_connect();
-      $statement = $db->prepare("select * from users;");
+      $statement = $db->prepare("select * from reminders;");
       $statement->execute();
       $rows = $statement->fetch(PDO::FETCH_ASSOC);
       return $rows;
     }
+
+  public function update_reminder () {
+    $db = db_connect();
+    // do update statement here
+  }
